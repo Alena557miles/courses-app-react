264534bb7a5ba0f0f422f3f4b8f0a13b5e91f040
Olena Nesterenko <554miles@gmail.com>
2022-12-06 11:22:10 +0200

    eslint

--- src/components/Courses/components/CourseCard/CourseCard.jsx
+++ src/components/Courses/components/CourseCard/CourseCard.jsx
@@ -3,13 +3,18 @@ import { Button } from '../../../../common/Button/Button';
 
 export function CourseCard(course) {
 	return (
+<<<<<<< Updated upstream
 		<li className='border border-green-400 p-5 mb-7 flex flex-row justify-between'>
 			<div className='flex flex-col max-w-5xl justify-evenly'>
+=======
+		<li className='border border-green-400 p-5 mb-7 flex justify-between align-center flex-row'>
+			<div className='flex flex-col max-w-3xl justify-evenly'>
+>>>>>>> Stashed changes
 				<h3 className='text-xl font-bold'>{course.course.title}</h3>
 				<p>{course.course.description}</p>
 			</div>
-			<div className='flex flex-col align-center max-w-2xl'>
-				<span className='font-bold'>Author:{course.course.name}</span>
+			<div className='flex flex-col align-center w-1/3 justify-between'>
+				<p className='font-bold'>Author: {course.course.name}</p>
 				<span className='font-bold'>Duration:{course.course.duration}</span>
 				<span className='font-bold'>Created:{course.course.creationDate}</span>
 				<Button
