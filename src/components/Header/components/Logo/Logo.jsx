2171760e36c4c6e9e94d3c845ae9922cd86a6c38
Alena557miles <557miles@gmail.com>
2022-12-06 21:01:01 +0200

    courses info finished

--- src/components/Header/components/Logo/Logo.jsx
+++ src/components/Header/components/Logo/Logo.jsx
@@ -4,7 +4,7 @@ import logo from './Logo.jpg';
 export function Logo() {
 	return (
 		<div className='box-border'>
-			<a href='#'>
+			<a href='https://uk.reactjs.org/tutorial/tutorial.html'>
 				<img src={logo} alt='Logo' className='max-w-xs h-20' />
 			</a>
 		</div>
