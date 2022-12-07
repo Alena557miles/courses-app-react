2590a795b60bc529e223160cf918a99bb6675c0a
Olena Nesterenko <554miles@gmail.com>
2022-12-07 19:19:30 +0200

    create course component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Header } from './components/Header/Header';
 function App() {
 	return (
 		<div className='container mx-auto pt-5'>
-			<Header />
+			<Header name={'Whyname'} />
 			<Course />
 		</div>
 	);
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Logo } from './components/Logo/Logo';
 import { Button } from '../../common/Button/Button';
 
-export function Header() {
+export function Header(props) {
+	const name = props.name;
 	return (
 		<div className='flex flex-row justify-between items-center border px-5 border-red-400'>
 			<Logo />
 			<div className='flex flex-row justify-between items-center gap-x-2'>
-				<p>Name</p>
+				<p>{name}</p>
 				<Button value={'Logout'} onClick={() => console.log('click')} />
 			</div>
 		</div>
