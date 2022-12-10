71dffef6242cfe3cda98425e1f620385e5b39486
Olena Nesterenko <554miles@gmail.com>
2022-12-10 13:13:35 +0200

    added overflow hidden to text with authors

--- src/components/Courses/components/CourseCard/CourseCard.jsx
+++ src/components/Courses/components/CourseCard/CourseCard.jsx
@@ -32,8 +32,8 @@ export function CourseCard(props) {
 						<h3 className='text-xl font-bold'>{course.title}</h3>
 						<p>{course.description}</p>
 					</div>
-					<div className='flex flex-col align-center w-1/3 justify-between'>
-						<p className='font-bold'>
+					<div className='flex flex-col align-center w-1/3 justify-between '>
+						<p className='font-bold truncate whitespace-nowrap overflow-hidden '>
 							Author:{' '}
 							<span className='font-normal'>{findAuthors(course.authors)}</span>
 						</p>
