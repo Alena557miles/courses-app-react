3c71c50c9bc9ec65f7d290e1305ba1b672c60350
Olena Nesterenko <554miles@gmail.com>
2022-12-11 17:37:37 +0200

    2ht

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Course } from './components/Courses/Courses';
 import { Header } from './components/Header/Header';
+import { Registration } from './components/Registration/Registration';
 
 function App() {
 	return (
 		<div className='container mx-auto pt-5'>
 			<Header name={'Whyname'} />
-			<Course />
+			{/* <Course /> */}
+			<Registration />
 		</div>
 	);
 }
