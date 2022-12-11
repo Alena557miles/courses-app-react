066e31ca255977fd2096f7d30c314d7c5dc40bce
Alena557miles <557miles@gmail.com>
2022-12-11 18:49:00 +0200

    ht2 layout created

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { Course } from './components/Courses/Courses';
 import { Header } from './components/Header/Header';
 import { Registration } from './components/Registration/Registration';
+import { Login } from './components/Login/Login';
+import { CourseInfo } from './components/CourseInfo/CourseInfo';
 
 function App() {
 	return (
-		<div className='container mx-auto pt-5'>
+		<div className='container mx-auto pt-5 h-screen'>
 			<Header name={'Whyname'} />
 			{/* <Course /> */}
-			<Registration />
+			{/* <Registration /> */}
+			{/* <Login /> */}
+			<CourseInfo />
 		</div>
 	);
 }
